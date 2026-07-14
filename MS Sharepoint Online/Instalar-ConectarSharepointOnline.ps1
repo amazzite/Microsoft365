5cacfca7efbe067938cb779cbe9f0548f5aa4241
Alejandro Mazzitelli <alejandro@mazzitelli.com.ar>
2026-07-14 16:58:35 -0300

    Sharepoint Online/Instalar-ConectarSharepointOnline.ps1

--- MS Sharepoint Online/Instalar-ConectarSharepointOnline.ps1
+++ MS Sharepoint Online/Instalar-ConectarSharepointOnline.ps1
@@ -12,5 +12,16 @@
 Install-Module Microsoft.Online.SharePoint.PowerShell -Scope CurrentUser
 Connect-SPOService -Url https://TU-TENANT-admin.sharepoint.com
 
+# Listar todos los sitios de SharePoint Online (Para probar la conexión)
+Get-SPOSite -Limit All
+
+# Obtener información de un sitio específico de SharePoint Online (Reemplaza la URL con la de tu sitio)
+Get-SPOSite -Identity https://empresa.sharepoint.com/sites/Contabilidad
+
+# Actualizar el módulo de SharePoint Online a la última versión
+Update-Module Microsoft.Online.SharePoint.PowerShell
+
+# Desconectar del servicio de SharePoint Online
+Disconnect-SPOService
 
 #Gracias por utilizar este script. Si tienes alguna pregunta o necesitas asistencia adicional, no dudes en contactarme a través de mi correo electrónico o visitar mi sitio web para más recursos y scripts relacionados con Microsoft 365.
